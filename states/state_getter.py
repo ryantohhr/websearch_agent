f449f54db8e5ba2050d4834e0399682cc798776c
Ryan Toh <ryantohhongrui@hotmail.com>
2025-08-14 00:13:01 +0800

    feat: finish state_getter function

--- states/state_getter.py
+++ states/state_getter.py
@@ -1,10 +1,28 @@
 def state_getter(state, key):
     match key:
+        case "planner_all":
+            return state["planner_response"]
         case "planner_latest":
             if state["planner_response"]:
                 return state["planner_response"][-1]
             else:
                 return state["planner_response"]
+
+        case "selector_all":
+            return state["selector_response"]
+        case "selector_latest":
+            if state["selector_response"]:
+                return state["selector_response"][-1]
+            else:
+                return state["selector_response"]
+
+        case "reporter_all":
+            return state["reporter_response"]
+        case "reporter_latest":
+            if state["reporter_response"]:
+                return state["reporter_response"][-1]
+            else:
+                return state["reporter_response"]
 
         case "reviewer_all":
             return state["reviewer_response"]
@@ -13,3 +31,27 @@ def state_getter(state, key):
                 return state["reviewer_response"][-1]
             else:
                 return state["reviewer_response"]
+
+        case "router_all":
+            return state["router_response"]
+        case "router_latest":
+            if state["router_response"]:
+                return state["router_response"][-1]
+            else:
+                return state["router_response"]
+
+        case "serper_all":
+            return state["serper_response"]
+        case "serper_latest":
+            if state["serper_response"]:
+                return state["serper_response"][-1]
+            else:
+                return state["serper_response"]
+
+        case "scraper_all":
+            return state["scraper_response"]
+        case "scraper_latest":
+            if state["scraper_response"]:
+                return state["scraper_response"][-1]
+            else:
+                return state["scraper_response"]
