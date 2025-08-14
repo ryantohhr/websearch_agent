d5bbe2f26e9f7a5439f7a8b5fe1e1ca6c45274d3
Ryan Toh <ryantohhongrui@hotmail.com>
2025-08-14 17:58:54 +0800

    fix: state containing duplicate responses

--- states/state_getter.py
+++ states/state_getter.py
@@ -1,57 +1,57 @@
 def state_getter(state, key):
     match key:
         case "planner_all":
-            return state["planner_response"]
+            return state.get("planner_response", [])
         case "planner_latest":
-            if state["planner_response"]:
-                return state["planner_response"][-1]
+            if state.get("planner_response", []):
+                return state.get("planner_response", [])[-1]
             else:
-                return state["planner_response"]
+                return state.get("planner_response", [])
 
         case "selector_all":
-            return state["selector_response"]
+            return state.get("selector_response", [])
         case "selector_latest":
-            if state["selector_response"]:
-                return state["selector_response"][-1]
+            if state.get("selector_response", []):
+                return state.get("selector_response", [])[-1]
             else:
-                return state["selector_response"]
+                return state.get("selector_response", [])
 
         case "reporter_all":
-            return state["reporter_response"]
+            return state.get("reporter_response", [])
         case "reporter_latest":
-            if state["reporter_response"]:
-                return state["reporter_response"][-1]
+            if state.get("reporter_response", []):
+                return state.get("reporter_response", [])[-1]
             else:
-                return state["reporter_response"]
+                return state.get("reporter_response", [])
 
         case "reviewer_all":
-            return state["reviewer_response"]
+            return state.get("reviewer_response", [])
         case "reviewer_latest":
-            if state["reviewer_response"]:
-                return state["reviewer_response"][-1]
+            if state.get("reviewer_response"):
+                return state.get("reviewer_response", [])[-1]
             else:
-                return state["reviewer_response"]
+                return state.get("reviewer_response", [])
 
         case "router_all":
-            return state["router_response"]
+            return state.get("router_response", [])
         case "router_latest":
-            if state["router_response"]:
-                return state["router_response"][-1]
+            if state.get("router_response", []):
+                return state.get("router_response", [])[-1]
             else:
-                return state["router_response"]
+                return state.get("router_response", [])
 
         case "serper_all":
-            return state["serper_response"]
+            return state.get("serper_response", [])
         case "serper_latest":
-            if state["serper_response"]:
-                return state["serper_response"][-1]
+            if state.get("serper_response", []):
+                return state.get("serper_response", [])[-1]
             else:
-                return state["serper_response"]
+                return state.get("serper_response", [])
 
         case "scraper_all":
-            return state["scraper_response"]
+            return state.get("scraper_response", [])
         case "scraper_latest":
-            if state["scraper_response"]:
-                return state["scraper_response"][-1]
+            if state.get("scraper_response", []):
+                return state.get("scraper_response", [])[-1]
             else:
-                return state["scraper_response"]
+                return state.get("scraper_response", [])
