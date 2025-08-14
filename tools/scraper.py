e40479164f2c64248e832b0a14400f588c5410c3
Ryan Toh <ryantohhongrui@hotmail.com>
2025-08-14 21:54:05 +0800

    fix: update scraper output so reporter can access links for citations

--- tools/scraper.py
+++ tools/scraper.py
@@ -8,10 +8,18 @@ def get_scraper_response(state: GraphState, get_selector_response):
     results = []
 
     for response in selector_response:
+        
         res = requests.get(response.link)
         soup = BeautifulSoup(res.text, "html.parser")
         text = " ".join(soup.stripped_strings())
-        results.append(text)
+
+        new_result = {
+             "title": response.title,
+             "link": response.link,
+             "text": text
+        }
+
+        results.append(new_result)
 
     if "scraper_response" not in state:
             state["scraper_response"] = [results]
